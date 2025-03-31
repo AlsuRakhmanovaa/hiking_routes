6bcec0ed024d1ec42b99578bbf1412127fe5ce43
Alsu <rakhmanova_2003@inbox.ru>
2025-03-31 16:02:03 +0300

    new header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import Header from './components/Header.jsx'
 import Places from './components/Places.jsx'
+import logoImg from './assets/logo.png'
 
 export default function App() {
 
   return (
     <>
-      <Header />
-      <Places />
+      <header>
+        <img src={logoImg} alt="" />
+        <h1>Hiking routes around the world!</h1>
+        <p>Create your personal collection of hiking places you would like to visit or you have visited.</p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          places
+          fallbackText="Select the places you would like to visit below."
+          onSelectPlace
+          isLoading
+          loadingText='Fetching your places...'
+        />
+      </main>
     </>
-  )
+  );
 }
